2ede02364a98aefb87d34540ed7c89163e984fa4
nguyenkhanhtung <tungnk0711@gmail.com>
2018-08-07 10:56:34 +0700

    commit project

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,19 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int salaryOfMonth;
+
+        System.out.println("Enter coefficients salary: ");
+        int coeff = scanner.nextInt();
+        System.out.println("Enter number of year: ");
+        int numOfYear = scanner.nextInt();
+
+
+    }
+}
